02ff8ec04d628a0a935418fc58099a6036d1f251
admin <admin@admin.com>
2018-02-06 16:09:32 +1100

    Add DELETE /todos/:id

--- server/server.js
+++ server/server.js
@@ -35,7 +35,7 @@ app.get('/todos/:id', (req, res) => {
   let id = req.params.id;
 
   if (!mongoose.Types.ObjectId.isValid(id)) {
-    return res.status(404).send('That ID is not valid');
+    return res.status(404).send();
   }
 
   Todo.findById(id).then((todo)=> {
@@ -45,6 +45,23 @@ app.get('/todos/:id', (req, res) => {
     res.send({todo});
   }).catch((e) => {
     res.status(400).send('That user couldn\'t be found');
+  });
+});
+
+app.delete('/todos/:id', (req, res) => {
+  let id = req.params.id;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findByIdAndRemove(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
   });
 });
 
