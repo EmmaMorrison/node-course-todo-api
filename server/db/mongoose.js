076a493b62b22b13d4f30fe7debd71b04ff61142
admin <admin@admin.com>
2018-02-09 18:33:34 +1100

    Setup separate test and development environments

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,8 @@
 let mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://EmmaMorrison:OrangePhone76@ds125288.mlab.com:25288/node-todo-app' || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGODB_URI);
 // mongoose.connect('mongodb://localhost:27017/TodoApp');
+//'mongodb://EmmaMorrison:OrangePhone76@ds125288.mlab.com:25288/node-todo-app'
 
 module.exports = {mongoose};
